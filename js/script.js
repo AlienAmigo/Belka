e0ade40939530fd59109cc936936199ebdff25b3
Pete <maestro_pete@inbox.ru>
2017-09-04 21:25:45 +0300

    Full Mobile Except Top Menu

--- js/script.js
+++ js/script.js
@@ -1,74 +1,128 @@
 $(document).ready(function(){
+// Switches
+function Accordeon() {
+	$(this).toggleClass('prices-accrod__item--active');
+	$('.prices-accrod__item').not(this).removeClass('prices-accrod__item--active');
+}
+	$('.prices-accrod__item').on('click', Accordeon);
+
+// Carousele	
 	$('.traces__list').slick({
-	mobileFirst: true,
 	dots: false,
-	arrows: true;
+	arrows: true,
+  prevArrow: '.btn-prev1',
+  nextArrow: '.btn-next1',
 	infinite: true,
+	mobileFirst: true,
 	speed: 300,
 	slidesToShow: 1,
 	slidesToScroll: 1,
-	responsive: [
-		{
-			breakpoint: 1280,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1,
-				infinite: true,
-				dots: false
-			}
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1
-			}
-		},
-		{
-			breakpoint: 320,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-		]
+	// responsive: [
+	// 	{
+	// 		breakpoint: 1280,
+	// 		settings: {
+	// 			slidesToShow: 3,
+	// 			slidesToScroll: 1,
+	// 			infinite: true,
+	// 			dots: false
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 768,
+	// 		settings: {
+	// 			slidesToShow: 3,
+	// 			slidesToScroll: 1
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 320,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1
+	// 		}
+	// 	}
+	// 	// You can unslick at a given breakpoint now by adding:
+	// 	// settings: "unslick"
+	// 	// instead of a settings object
+	// 	]
 	});
+	
 	$('.photo__list').slick({
 	dots: false,
+	arrows: true,
+  prevArrow: '.btn-prev2',
+  nextArrow: '.btn-next2',
 	infinite: true,
+	mobileFirst: true,
 	speed: 300,
 	slidesToShow: 1,
 	slidesToScroll: 1,
-	responsive: [
-		{
-			breakpoint: 1280,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1,
-				infinite: true,
-				dots: false
-			}
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1
-			}
-		},
-		{
-			breakpoint: 320,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-		]
+	// responsive: [
+	// 	{
+	// 		breakpoint: 1280,
+	// 		settings: {
+	// 			slidesToShow: 3,
+	// 			slidesToScroll: 1,
+	// 			infinite: true,
+	// 			dots: false
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 768,
+	// 		settings: {
+	// 			slidesToShow: 3,
+	// 			slidesToScroll: 1
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 320,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1
+	// 		}
+	// 	}
+	// 	// You can unslick at a given breakpoint now by adding:
+	// 	// settings: "unslick"
+	// 	// instead of a settings object
+	// 	]
+	});
+	$('.reviews__list').slick({
+	dots: false,
+	arrows: true,
+  prevArrow: '.btn-prev3',
+  nextArrow: '.btn-next3',
+	infinite: true,
+	mobileFirst: true,
+	speed: 300,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	// responsive: [
+	// 	{
+	// 		breakpoint: 1280,
+	// 		settings: {
+	// 			slidesToShow: 3,
+	// 			slidesToScroll: 1,
+	// 			infinite: true,
+	// 			dots: false
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 768,
+	// 		settings: {
+	// 			slidesToShow: 3,
+	// 			slidesToScroll: 1
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 320,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1
+	// 		}
+	// 	}
+	// 	// You can unslick at a given breakpoint now by adding:
+	// 	// settings: "unslick"
+	// 	// instead of a settings object
+	// 	]
 	});
 });
